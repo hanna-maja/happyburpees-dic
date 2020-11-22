a24da2e1d73e66479ceb10b1790444725696cbf7
hanna-maja <hanna-maja@sewen.se>
2020-11-22 15:53:55 +0000

    STYLE: Set active menu item

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,29 @@ $("#search").keyup(function() {
         $( "#dictionary-items" ).html(data);
     });
 });
+
+function setNavigation() {
+    var path = window.location.pathname;
+    path = path.replace(/\/$/, "");
+    path = decodeURIComponent(path);
+    $("#sidebar-wrapper a").each(function () {
+        var href = $(this).attr('href');
+        if (href == '/'){ // Exception for startpage
+            if(path == ''){
+                $(this).addClass('active');
+            }
+            else{
+                $(this).removeClass('active');
+            }
+        }
+        else if (path.substring(0, href.length) === href) {
+            $(this).addClass('active');
+        }
+        else{
+            $(this).removeClass('active');
+        }
+    });
+}
+$(document).ready(function(){
+    setNavigation();
+});
