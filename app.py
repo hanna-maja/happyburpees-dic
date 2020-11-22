5bd07571750754290764650eb097a72f3475bd78
hanna-maja <hanna-maja@sewen.se>
2020-11-22 15:52:23 +0000

    FIX: Route for empty search

--- app.py
+++ app.py
@@ -161,4 +161,4 @@ def update_item(id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
